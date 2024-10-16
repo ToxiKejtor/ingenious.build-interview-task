8785947626b552711fd59ba7455f2c6ee7c7a873
Maciej Tokarczyk <maciek.tokarczyk1@gmail.com>
2024-10-16 16:01:00 +0200

    add views

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
-    <div></div>
+  <h1>Timetable</h1>
+  <nav>
+    <RouterLink to="/">Lines</RouterLink>
+    <RouterLink to="/stops">Stops</RouterLink>
+  </nav>
+  <RouterView />
 </template>
+<script setup lang="ts"></script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import LinesView from "@/pages/LinesView.vue";
+import StopsView from "@/pages/StopsView.vue";
 
-const routes: Array<RouteRecordRaw> = []
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    component: LinesView
+  },
+  {
+    path: '/stops',
+    component: StopsView
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
